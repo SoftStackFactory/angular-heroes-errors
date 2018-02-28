ae91746854c60148ff61372bc5c8efcd8d2ad72f
Peter Horton <phorton@softstackfactory.org>
2018-02-28 23:30:56 +0000

    imported message service into hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,7 +3,7 @@ import { Hero } from './Hero';
 import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-import { MessageService } from './message.service';
+import { MessageService } from '../../message.service';
 
 @Injectable()
 export class HeroService {
