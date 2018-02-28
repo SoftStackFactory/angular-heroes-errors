8563c0c46c6450f0490c164246cd91e7530f32c3
Peter Horton <phorton@softstackfactory.org>
2018-02-28 23:27:20 +0000

    message service updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from './message.service';
+import { MessageService } from '../message.service';
 
 
 @NgModule({
