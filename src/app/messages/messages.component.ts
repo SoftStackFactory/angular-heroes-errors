7a7f54a09cafb8d2da7cafc987cdbb29fe304d3f
Peter Horton <phorton@softstackfactory.org>
2018-03-01 01:33:57 +0000

    messages.compoent.ts property messageService needs to be assigned to the imported MessageService

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -8,7 +8,7 @@ import { MessageService} from '../message.service';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor(public messageService:) { }
+  constructor(public  messageService:) { }
 
   ngOnInit() {
   }
