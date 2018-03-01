7368b8328f7c4904bf44fcecfc15d4c8ac671ed2
Peter Horton <phorton@softstackfactory.org>
2018-03-01 00:32:20 +0000

    created a heroservice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 
 
