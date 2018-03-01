ba822e76f8b608353035cdce2be7f03b67ff7a8e
Peter Horton <phorton@softstackfactory.org>
2018-03-01 00:50:10 +0000

    a new here detail component was created but imported wrong

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from '../message.service';
+import { MessageService } from './message.service';
 
 
 @NgModule({
