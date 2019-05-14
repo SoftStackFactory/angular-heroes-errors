dbf0122635254ee4c098d982a6a44cca598cc261
Peter Horton <phorton@softstackfactory.com>
2019-05-14 19:41:48 +0000

    revereted changes

--- git-branch.sh
+++ git-branch.sh
@@ -2,7 +2,7 @@
 for BRANCH in `git branch -a --list|sed 's/\*//g'`;
   do 
     git checkout $BRANCH
-    git fetch
-    git branch --set-upstream-to=origin/$BRANCH $BRANCH
+    git merge master
+   
   done
 git checkout master;
