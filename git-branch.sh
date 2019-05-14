adb68b1e9802a4576299c7ffa39abf4f853bd6d0
Peter Horton <phorton@softstackfactory.com>
2019-05-14 21:30:48 +0000

    updated git branch

--- git-branch.sh
+++ git-branch.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-for BRANCH in `git branch -a --list|sed 's/\*//g'`;
+
+# git merge or rebase all branches --a for all 
+for BRANCH in `git branch  --list|sed 's/\*//g'`;
   do 
-    git checkout $BRANCH
-    git merge master
+  echo ${BRANCH}
+    #git checkout $BRANCH
+    #git merge master
    
   done
 git checkout master;
+
+# pulls all remote branches and creates local
+for b in `git branch -r | grep -v -- '->'`;
+  do 
+    git branch --track ${b##origin/} $b; 
+  done
